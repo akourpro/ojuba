2f76591f1aa5cdb0dfd3de2e3ba3a1d83d01602e
Mohammad Akour <pro.akour@gmail.com>
2026-08-22 03:34:21 +0300

    اصلاحات عامة

--- errors/301.php
+++ errors/301.php
@@ -1,4 +1,11 @@
 <?php
+
+$__d = __DIR__;
+while (!is_file($__d . '/autoload.php') && $__d !== dirname($__d)) {
+    $__d = dirname($__d);
+}
+require_once $__d . '/autoload.php';
+
 echo safeRender('404.twig', [
     "error_type" => "301",
     "error_message" => $lang['error_301'],
--- errors/404.php
+++ errors/404.php
@@ -1,4 +1,11 @@
 <?php
+
+$__d = __DIR__;
+while (!is_file($__d . '/autoload.php') && $__d !== dirname($__d)) {
+    $__d = dirname($__d);
+}
+require_once $__d . '/autoload.php';
+
 echo safeRender('404.twig', [
     "error_type" => "404",
     "error_message" => $lang['page_not_found'],
